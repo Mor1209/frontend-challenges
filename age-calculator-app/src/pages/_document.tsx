d678ae297eba4a57fa26bfff3320b6fa13439167
moritzHauptmann <mohauptmann@gmail.com>
2023-04-18 17:59:26 +1000

    install next.js with tailwind

---
+++ age-calculator-app/src/pages/_document.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @next/next/no-title-in-document-head */
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang='en'>
+      <Head>
+        <title>Frontend Mentor | Age calculator app</title>
+        <meta property='title' content='My page title' key='title' />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
---
+++ age-calculator-app/src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
+        Day DD Month MM Year YYYY -- years -- months -- days
+      </div>
+    </main>
+  )
+}
